50c50d41e45ae51209ee9088e4deb0c8def36c66
juhyunchoi <julia.juhyun@gmail.com>
2024-05-29 20:19:56 +0900

    gitignore and firebase are not saved

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmjtK_fUZv3Ehgw7jy3-Y5hu_FeV8ERbo",
+  authDomain: "pickstudent-87a3e.firebaseapp.com",
+  projectId: "pickstudent-87a3e",
+  storageBucket: "pickstudent-87a3e.appspot.com",
+  messagingSenderId: "588519731656",
+  appId: "1:588519731656:web:36e1e46a36878b21fed0fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
